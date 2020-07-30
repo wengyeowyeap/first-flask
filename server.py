be5185586cc9a0ac331d9d6488601dd492075df8
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-07-30 16:53:42 +0800

    added non-functioning contact us form

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-
+    first_name = request.args.get('first_name')
+    return render_template('index.html', first_name=first_name)
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
-
 
 if __name__ == '__main__':
     app.run()
