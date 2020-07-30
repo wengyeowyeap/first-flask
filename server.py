1d0fb2f6a303657ddb4d82e90b0c7cb73c179785
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-07-30 16:23:12 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-   return render_template('index.html') # by default looks for index.html inside a templates folder in the same directory as this script.
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
-if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-   app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
