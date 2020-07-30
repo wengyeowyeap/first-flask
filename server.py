a82a03b84e1c25bf92ef947de595cd518caa0159
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-07-30 16:41:07 +0800

    refactored templates to use a master layout

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    signed_in = False # we are hardcoding this just to demonstrate how we can do conditionals in our template files, in future we won't be hardcoding this.
-    return render_template('index.html', signed_in=signed_in)
+    return render_template('index.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
